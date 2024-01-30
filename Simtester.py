72afea284113290423e4dfa9a563798355d316b1
innovatte-admin <utkuce7@gmail.com>
2024-01-30 17:58:26 +0300

    Simstester updated. Terminal output is more explanatory

--- Simtester.py
+++ Simtester.py
@@ -1,9 +1,11 @@
 import Simdemo
-inp = input("Enter number: ")
-oldVal = int(inp)
-while(inp != "q"):
-    inp = int(inp)
-    newVal= Simdemo.SimulatorInPercent(inp,oldVal,10)
-    print("input:" , inp, "\t oldVal: " ,oldVal, "\t newVal:", newVal, "\t offVal: ", inp/10)
-    oldVal = newVal
-    inp = input("Enter number: ")
+inp = input("Enter a number: \nPress q to quit\n")
+if inp != "q":
+    oldVal = int(inp)
+    while inp != "q":
+        inp = int(inp)
+        newVal= Simdemo.SimulatorInPercent(inp,oldVal,10)
+        print("input:" , inp, "\t oldVal: " ,oldVal, "\t newVal:", newVal, "\t offVal: ", inp/10)
+        oldVal = newVal
+        inp = input("Enter number: ")
+print("\nProgram ends now.\n")
