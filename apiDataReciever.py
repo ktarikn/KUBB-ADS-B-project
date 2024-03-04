c832f806c52ef03d9f761d82d68467b78223d7a1
Betül Sağlam <betul.saglam004@gmail.com>
2024-03-04 21:13:52 +0300

    tryin some things

--- apiDataReciever.py
+++ apiDataReciever.py
@@ -1,26 +1,32 @@
-#IMPORTING LIBRARY
+# IMPORTING LIBRARY
 import requests
 import json
 import pandas as pd
+from datetime import datetime
 
-#AREA EXTENT COORDINATE WGS4
-lon_min,lat_min=-125.974,30.038
-lon_max,lat_max=-68.748,52.214
+# REST API QUERY
+url_data = "https://betulls:481projesi@opensky-network.org/api/states/all?lamin=35.902&lomin=25.909&lamax=42.026&lomax=44.574"
+response = requests.get(url_data).json()
 
-#REST API QUERY
-user_name=''
-password=''
-#url_data='https://'+user_name+':'+password+'@opensky-network.org/api/states/all?'+'lamin='+str(lat_min)+'&lomin='+str(lon_min)+'&lamax='+str(lat_max)+'&lomax='+str(lon_max)
-url_data= "https://opensky-network.org/api/states/all?lamin=30.038&lomin=-125.974&lamax=52.214&lomax=-68.748"
-response=requests.get(url_data).json()
+# LOAD TO PANDAS DATAFRAME
+col_name = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'long', 'lat', 'baro_altitude',
+            'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude', 'squawk', 'spi',
+            'position_source']
 
-#LOAD TO PANDAS DATAFRAME
-col_name=['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground','velocity',
-'true_track','vertical_rate','sensors','geo_altitude','squawk','spi','position_source']
-flight_df=pd.DataFrame(response['states'])
-flight_df=flight_df.loc[:,0:16]
-flight_df.columns=col_name
-flight_df=flight_df.fillna('No Data') #replace NAN with No Data
-flight_df.head()
+data = response['states']
+
+# Convert unix timestamps to a human-readable format
+for i in range(len(data)):
+    date = datetime.utcfromtimestamp(data[i][3])
+    data[i][3] = date.strftime("%Y-%m-%d %H:%M:%S UTC")
+
+flight_df = pd.DataFrame(data)
+flight_df = flight_df.loc[:, 0:16]
+flight_df.columns = col_name
+flight_df = flight_df.fillna('No Data')  # replace NAN with No Data
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+flight_df = flight_df.sort_values(by='time_position')
 print(flight_df)
-
