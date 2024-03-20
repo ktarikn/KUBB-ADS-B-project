bb452bbc9e8e8752324b1f9b7be660f49b83775c
uUtkuC <utkuce7@gmail.com>
2024-03-20 14:25:01 +0300

    added CI for this python Project

---
+++ test_main.py
@@ -0,0 +1,13 @@
+from PlaneData import PlaneData
+
+
+
+plane_instance = PlaneData("471f8f", "WZZ4641", "1710933062", "27.623", "40.4816", "False", "239.4", "158.18", "0")
+
+
+def test_isInit_correct():
+    assert (plane_instance.longitude =="27.623")
+
+def test_is_update_functioning():
+    plane_instance.update_data(longitude="28.000",latitude="40.4800",on_ground="False",true_track="160.00",velocity="300") #a change is made
+    assert (plane_instance.longitude == "28.000")
