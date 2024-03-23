8c3523275e88b77445e5e34de1959dafef069ebb
uUtkuC <utkuce7@gmail.com>
2024-03-23 18:42:23 +0300

    updated test

--- test_main.py
+++ test_main.py
@@ -6,8 +6,8 @@ plane_instance = PlaneData("471f8f", "WZZ4641", "1710933062", "27.623", "40.4816
 
 
 def test_isInit_correct():
-    assert (plane_instance.location_history[plane_instance.idx][1] =="27.623")
+    assert (plane_instance.location_history[0][1] =="27.623")
 
 def test_is_update_functioning():
     plane_instance.update_data(longitude="28.000",latitude="40.4800",on_ground="False",true_track="160.00",velocity="300") #a change is made
-    assert (plane_instance.location_history[plane_instance.idx][1] =="28.000")
+    assert (plane_instance.location_history[1][1] =="28.000")
