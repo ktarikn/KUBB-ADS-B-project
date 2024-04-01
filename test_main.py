22220f843370435cc71406c9dd241789aba52fbe
uUtkuC <utkuce7@gmail.com>
2024-04-01 15:33:17 +0300

    updated tests

--- test_main.py
+++ test_main.py
@@ -2,12 +2,12 @@ from PlaneData import PlaneData
 import numpy as np
 
 
-plane_instance = PlaneData("471f8f", "WZZ4641", "1710933062", "27.623", "40.4816", "False", "239.4", "158.18", "0")
+plane_instance = PlaneData("471f8f", "WZZ4641", "1710933062", 27.623, 40.4816, "False", "239.4", "158.18", "0")
 
 
 def test_isInit_correct():
     assert (plane_instance.location_history[0][1] ==27.623)
 
 def test_is_update_functioning():
-    plane_instance.update_data(longitude="28.000",latitude="40.4800",on_ground="False",true_track="160.00",velocity="300") #a change is made
+    plane_instance.update_data(longitude=28.000,latitude=40.4800,on_ground="False",true_track="160.00",velocity="300") #a change is made
     assert (plane_instance.location_history[1][1] ==28.000)
